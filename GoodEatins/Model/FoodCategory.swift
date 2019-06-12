98738df1ebea79a847c29023934e38b547ae3af3
Irvin Reyes <reyes.yerp@gmail.com>
2019-06-12 18:22:33 -0500

    Project setup and datamodel

---
+++ GoodEatins/Model/FoodCategory.swift
@@ -0,0 +1,14 @@
+//
+//  FoodCategory.swift
+//  GoodEatins
+//
+//  Created by iv on 6/12/19.
+//  Copyright © 2019 ivsath. All rights reserved.
+//
+
+import Foundation
+
+struct FoodCategory {
+    let title: String
+    let imageName: String
+}
---
+++ GoodEatins/Model/Recipe.swift
@@ -0,0 +1,15 @@
+//
+//  Recipe.swift
+//  GoodEatins
+//
+//  Created by iv on 6/12/19.
+//  Copyright © 2019 ivsath. All rights reserved.
+//
+
+import Foundation
+
+struct Recipe {
+    let title: String
+    let instructions: String
+    let imageName: String
+}
